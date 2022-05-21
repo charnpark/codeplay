d10b2fab78596276236c48c7a0455300260aed7d
charnpark <geimhaneunt513@gmail.com>
2022-05-21 12:35:20 +0900

    5/21 박율ㅈㄴ못생김 똥피하기게임 완성및 설명완료

--- pygame/06_ddong.py
+++ pygame/06_ddong.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-import cgi
 import pygame
+import random
 pygame.init()
 screen_width = 480 #가로
 screen_height = 640 #세로
@@ -18,26 +18,18 @@ character_width = character_size[0]
 character_height = character_size[1]
 character_xPos = (screen_width / 2) - (character_width / 2)
 character_yPos = screen_height - character_height
-# character_xPos = (screen_width) - (character_width)
-# character_yPos = screen_height - character_height
-# character_xPos = 0
-# character_yPos = 0
-# character_xPos = (screen_width) - (character_width)
-# character_yPos = 0
-# character_xPos = 0
-# character_yPos = screen_height - character_height
-# character_xPos = (screen_width) - (character_width)
-# character_yPos = screen_height - character_height
+to_x = 0
+# to_y = 0
 
 enemy = pygame.image.load("pygame/source/enemy.png")
 enemy_size = enemy.get_rect().size
 enemy_width = enemy_size[0]
 enemy_height = enemy_size[1]
-enemy_xPos = (screen_width / 2) - (enemy_width / 2)
-enemy_yPos = (screen_height / 2) - (enemy_height / 2)
+enemy_xPos = random.randint(0, (screen_width - enemy_width))
+enemy_yPos = 0
 
-to_x = 0
-to_y = 0
+enemy_speed = 1
+
 
 running = True 
 while running:
@@ -48,20 +40,20 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:      
                 to_x -= 1
+            # elif event.key == pygame.K_DOWN:
+            #     to_y += 1
             elif event.key == pygame.K_RIGHT:
                 to_x += 1
-            elif event.key == pygame.K_UP:
-                to_y -= 1
-            elif event.key == pygame.K_DOWN:
-                to_y += 1
+            # elif event.key == pygame.K_UP:
+            #     to_y -= 1
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 to_x = 0
-            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                to_y = 0
+            # elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+            #     to_y = 0
 
     character_xPos += to_x * dt
-    character_yPos += to_y * dt
+    # character_yPos += to_y * dt
 
     if character_xPos < 0:
         character_xPos = 0
@@ -72,6 +64,12 @@ while running:
         character_yPos = 0
     elif character_yPos > screen_height - character_height:
         character_yPos = screen_height - character_height
+
+    enemy_yPos += enemy_speed
+    if enemy_yPos > screen_height:
+        enemy_yPos = enemy_height * -1
+        enemy_xPos = random.randint(0, (screen_width - enemy_width))
+        enemy_speed = random.randint(5, 15)
 
     character_rect = character.get_rect()
     character_rect.left = character_xPos
